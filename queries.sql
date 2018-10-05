6e192659f4d45fac35a657d23ca9aa8470559766
poojakolan <lakshmipoojak@gmail.com>
2018-10-05 08:50:53 +0800

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,5 @@
+queries:
+
+select foundry.foundry_name, apps.name,apps.memory,apps.instances,apps.disk_space,apps.state,apps.cpu_used,apps.memory_used,apps.disk_used, spaces.space_name,orgs.org_name from grafana.foundries as foundry, grafana.pcf_apps as apps, grafana.pcf_org as orgs, grafana.pcf_space as spaces where apps.space_id = spaces.id and spaces.org_id = orgs.id and orgs.foundry_id = foundry.id and foundry.foundry_name in (${foundry_list}) and orgs.org_name in (${ORG_NAMES});
+
+select foundry.foundry_name, apps.name,apps.memory,apps.instances,apps.disk_space,apps.state,apps.cpu_used,apps.memory_used,apps.disk_used, spaces.space_name,orgs.org_name from grafana.foundries as foundry, grafana.pcf_apps as apps, grafana.pcf_org as orgs, grafana.pcf_space as spaces where apps.space_id = spaces.id and spaces.org_id = orgs.id and orgs.foundry_id = foundry.id and foundry.foundry_name;
