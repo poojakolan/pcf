b9e6f77ac882dbcaf9670c6ea635d873af3851c4
poojakolan <lakshmipoojak@gmail.com>
2018-10-14 11:16:34 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -19,3 +19,6 @@ select foundry.foundry_name, apps.name,apps.memory,apps.instances,apps.disk_spac
 
 
 select foundry.foundry_name, apps.name,apps.memory,apps.instances,apps.disk_space,apps.state,apps.cpu_used,apps.memory_used,apps.disk_used, spaces.space_name,orgs.org_name from grafana.foundries as foundry, grafana.pcf_apps as apps, grafana.pcf_org as orgs, grafana.pcf_space as spaces where apps.space_id = spaces.id and spaces.org_id = orgs.id and orgs.foundry_id = foundry.id and foundry.foundry_name in (${foundry_list}) and orgs.org_name in (${ORG_NAMES}) order by apps.memory_used desc, apps.state limit 10;
+
+
+select apps.memory_used as memory, UNIX_TIMESTAMP(apps.updated) as time_sec from grafana.foundries as foundry, grafana.pcf_apps as apps, grafana.pcf_org as orgs, grafana.pcf_space as spaces where apps.space_id = spaces.id and spaces.org_id = orgs.id and orgs.foundry_id = foundry.id;
